3fe12b4415176e74741beb9e81b550251be878f4
m-vishnuraj <mvishnurajm@gmail.com>
2024-06-25 19:38:59 +0530

    Data Type Config

--- script.js
+++ script.js
@@ -1,3 +1,39 @@
-console.log("Hello World")
-console.warn("Hello World")
-console.error("Hello World")
+console.log("Hello World");
+console.warn("Hello World");
+console.error("Hello World");
+
+
+var a = 10;
+console.log(a);
+var a = "Name"
+console.log(a);
+
+// let name = "john";
+// console.log(name);
+// let name
+
+const pi = 3.14;
+console.log(pi);
+// const pi = 3.145
+
+// Nan
+
+let valueOne = 2;
+let valueTwo = "a";
+
+console.log(valueOne + valueTwo);
+
+console.log(valueOne + parseInt(valueTwo));
+
+
+const name = "John";
+const age = 30;
+const rating = 4.5;
+const isCool = true;
+const x = null;
+var y;
+
+console.log(typeof name);
+
+console.log('My name is ' + name + ' and I am ' + age);
+console.log(`My name is ${name} and I am ${age}`);
