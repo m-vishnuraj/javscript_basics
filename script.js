12ba481b9cd14e3949be54fe8f8e967fe32a4a02
Akash-thilakan <akashthilakan2462003@gmil.com>
2024-06-30 22:47:12 +0530

    day 3

--- script.js
+++ script.js
@@ -1,190 +1,126 @@
-// console.log("Hello World");
-// console.warn("Hello World");
-// console.error("Hello World");
 
 
-// var a = 10;
-// console.log(a);
-// var a = "Name"
-// console.log(a);
+// console.log("Function");
 
-// // let name = "john";
-// // console.log(name);
-// // let name
-
-// const pi = 3.14;
-// console.log(pi);
-// // const pi = 3.145
-
-// // Nan
-
-// let valueOne = 2;
-// let valueTwo = "a";
-
-// console.log(valueOne + valueTwo);
-
-// console.log(valueOne + parseInt(valueTwo));
-
-
-// const name = "John";
-// const age = 30;
-// const rating = 4.5;
-// const isCool = true;
-// const x = null;
-// var y;
-
-// console.log(typeof name);
-
-// console.log('My name is ' + name + ' and I am ' + age);
-// console.log(`My name is ${name} and I am ${age}`);
-
-
-// // Array
-
-// arrayOne = ["john", 30, true, null, undefined]
-
-// console.log(arrayOne);
-
-// // push
-
-// arrayOne.push("Apple");
-// console.log(arrayOne);
-
-// // pop
-// arrayOne.pop();
-// console.log(arrayOne);
-// // unshift
-
-// arrayOne.unshift("Mango");
-// console.log(arrayOne);
-
-// console.log(arrayOne[0]);
-
-// arrayOne[3] = "banana"
-// console.log(arrayOne);
-
-// console.log(arrayOne.length);
-// arrayOne.splice(3, 2);
-// console.log(arrayOne);
-
-
-// let valueOnee = "2"
-// let valueTwoo = 2;
-
-// console.log(valueOnee == valueTwoo);
-// console.log(valueOnee === valueTwoo);
-
-
-// // Conditional Statements
-
-// // if
-
-// let value = 15;
-
-// if (value >= 18) {
-//     console.log("You are Adult")
+// function showSting() {
+//     console.log("Hello World");
 // }
 
-// let valuee = 16;
+// showSting();
 
-// if (valuee >= 18) {
-//     console.log("You are adult");
-// } else {
-//     console.log("You are a minor");
+// var showString = function () {
+//     console.log("Hello World");
 // }
 
-// // ternary 
+// showSting();
 
-// let age = 20;
 
-// let type = (age >= 18) ? "Adult" : "Minor"
+// var showSting = () => {
+//     console.log(" Arrow Function Hello World");
+// }
 
-// console.log(type);
+// showSting();
 
+
+// // Map
+
+// var number = [1, 2, 3, 4, 5];
+
+
+// var square = number.map((num) => {
+//     return num * num;
+// })
+
+// console.log(square);
+
+// var fruits = ["Apple", "Banana", "Mango"];
+
+// var upperCase = fruits.map((fruit) => {
+//     return fruit.toUpperCase();
+// });
+
+// console.log(upperCase);
+
+// // Enhanced Object Literals
+
+// var name = "John";
+// var age = 20;
+
+// var person = {
+//     name,
+//     age,
+//     isStudent: true
+// }
+
+// console.log(person);
+
+// // Destructuring
+
+// var person = {
+//     name: "Akhil",
+//     age: 20,
+//     isStudent: true
+// }
+// var { name, age, isStudent } = person;
+
+// console.log(name);
+// console.log(age);
+// console.log(isStudent);
+
+// // promises
+
+// var promise = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         resolve("Promise resolved")
+//     }, 2000);
+// })
+
+// promise.then((reslut) => {
+//     console.log(reslut);
+// })
+
+
+// // ! Spread Operator
+
+// var numbers = [1, 2, 3, 4, 5];
+
+// var newNumber = [...numbers, 6, 7, 8];
+
+// console.log(newNumber);
 console.log("Function");
+// function showsting()
+// {
+//         console.log("Hello World");
+// } 
+// showsting();
+// var showstring = function()
+// {
+//         console.log("hello world");
+// }
+// showsting();
+// var showSting = () =>{
+//     console.log("Arrow hello world");
+// }
+// showSting();
+// //map
+// var numbers = [1,2,3,4,5]
+// var square = numbers.map((num)=>
+//     {
+//         return num * num;
+//     })
+//     console.log(square);
 
-function showSting() {
-    console.log("Hello World");
-}
+//     var fruits = ["apple","banana","mango"];
+//     var upperCase = fruits.map((fruit) => {
+//         return fruit.toUpperCase();
+//     })
+//     console.log(upperCase);
 
-showSting();
-
-var showString = function () {
-    console.log("Hello World");
-}
-
-showSting();
-
-
-var showSting = () => {
-    console.log(" Arrow Function Hello World");
-}
-
-showSting();
-
-
-// Map
-
-var number = [1, 2, 3, 4, 5];
-
-
-var square = number.map((num) => {
-    return num * num;
-})
-
-console.log(square);
-
-var fruits = ["Apple", "Banana", "Mango"];
-
-var upperCase = fruits.map((fruit) => {
-    return fruit.toUpperCase();
-});
-
-console.log(upperCase);
-
-// Enhanced Object Literals
-
-var name = "John";
-var age = 20;
-
+var name = "akash";
+var age = 21;
 var person = {
     name,
     age,
-    isStudent: true
-}
-
+    IsStudent: true
+} 
 console.log(person);
-
-// Destructuring
-
-var person = {
-    name: "Akhil",
-    age: 20,
-    isStudent: true
-}
-var { name, age, isStudent } = person;
-
-console.log(name);
-console.log(age);
-console.log(isStudent);
-
-// promises
-
-var promise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        resolve("Promise resolved")
-    }, 2000);
-})
-
-promise.then((reslut) => {
-    console.log(reslut);
-})
-
-
-// ! Spread Operator
-
-var numbers = [1, 2, 3, 4, 5];
-
-var newNumber = [...numbers, 6, 7, 8];
-
-console.log(newNumber);
